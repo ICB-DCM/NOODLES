83d6f2915a6456b4f1cafb44d5ddbbdba3006406
yannikschaelte <yannik.schaelte@gmail.com>
2018-03-12 23:20:36 +0100

    improve doc

--- +noodles/NoodleOptions.m
+++ +noodles/NoodleOptions.m
@@ -35,9 +35,16 @@ classdef NoodleOptions
     methods
         
         function options = NoodleOptions(options_in)
-            if isa(options_in,'noodles.NoodleOptions')
+            % Constructor for NoodleOptions.
+            %
+            % Input:
+            % options_in: NoodleOptions instance or struct having as fields
+            %             a subset of the NoodleOptions properties.
+            %             Validity of all fields is checked.
+            
+            if isa(options_in, 'noodles.NoodleOptions')
                 options = options_in;
-            elseif isa(options_in,'struct')
+            elseif isa(options_in, 'struct')
                 fields_in = fields(options_in);
                 for jf = 1:length(fields_in)
                     if ~isprop(options, fields_in{jf})
